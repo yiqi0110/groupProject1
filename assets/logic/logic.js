3f362e76bc59b106dc9d2d569c7c4ffc39c3a028
yiqi0110 <lili-rovert@hotmail.com>
2018-08-28 21:25:27 -0600

    j

---
+++ assets/logic/logic.js
@@ -0,0 +1,42 @@
+// anything we want to have happen before the page starts
+//===========================================================================
+
+
+// variable declarations
+//===========================================================================
+
+
+// function declarations
+//===========================================================================
+
+// firebase stuff
+
+// api calls
+
+// if no firebase data, prompt profile creation
+
+// post new user info to firebase
+
+// giphy api button stuff
+
+// translator api stuff
+
+
+// function functionality
+//===========================================================================
+
+// sign in or create profile or anonymous profile
+
+// sign in ==> input username and password
+
+// create profile ==> set username and password, and set language
+
+// anonymous ==> set language
+
+// on.click submit to translator as well as submit text to chat
+
+// giphy stuff from last project in a scroll menu to choose from
+
+// gif post into the chatroom
+
+
