f5ea32a66c94c3e4820d3e81b3aca957f162fb75
yiqi0110 <lili-rovert@hotmail.com>
2018-08-31 13:11:49 -0600

    updates to the api

--- assets/logic/logic.js
+++ assets/logic/logic.js
@@ -17,19 +17,445 @@
 
 // post new user info to firebase
 
-// giphy api button stuff
+// giphy api button stuff  
+var supportedLanguages = [
+    {
+        languageNames: "Afrikaans",
+        symbol: "af"
+    },
+    {
+        languageNames: "Albanian",
+        symbol: "sq"
+    },
+    {
+        languageNames: "Amharic",
+        symbol: "am"
+    },
+    {
+        languageNames: "Arabic",
+        symbol: "ar"
+    },
+    {
+        languageNames: "Armenian",
+        symbol: "hy"
+    },
+    {
+        languageNames: "Azeerbaijani",
+        symbol: "az"
+    },
+    {
+        languageNames: "Basque",
+        symbol: "eu"
+    },
+    {
+        languageNames: "Belarusian",
+        symbol: "be"
+    },
+    {
+        languageNames: "Bengali",
+        symbol: "bn"
+    },
+    {
+        languageNames: "Bosnian",
+        symbol: "bs"
+    },
+    {
+        languageNames: "Bulgarian",
+        symbol: "bg"
+    },
+    {
+        languageNames: "Catalan",
+        symbol: "ca"
+    },
+    {
+        languageNames: "Cebuano",
+        symbol: "ceb"
+    },
+    {
+        languageNames: "Chinese(Simplified)",
+        symbol: "zh-CN"
+    },
+    {
+        languageNames: "Chinese(Traditional)",
+        symbol: "zh-TW"
+    },
+    {
+        languageNames: "Corsican",
+        symbol: "co"
+    },
+    {
+        languageNames: "Croatian",
+        symbol: "hr"
+    },
+    {
+        languageNames: "Czech",
+        symbol: "cs"
+    },
+    {
+        languageNames: "Danish",
+        symbol: "da"
+    },
+    {
+        languageNames: "Dutch",
+        symbol: "nl"
+    },
+    {
+        languageNames: "English",
+        symbol: "en"
+    },
+    {
+        languageNames: "Esperanto",
+        symbol: "eo"
+    },
+    {
+        languageNames: "Estonian",
+        symbol: "et"
+    },
+    {
+        languageNames: "Finnish",
+        symbol: "fi"
+    },
+    {
+        languageNames: "French",
+        symbol: "fr"
+    },
+    {
+        languageNames: "Frisian",
+        symbol: "fy"
+    },
+    {
+        languageNames: "Galician",
+        symbol: "gl"
+    },
+    {
+        languageNames: "Georgian",
+        symbol: "ka"
+    },
+    {
+        languageNames: "German",
+        symbol: "de"
+    },
+    {
+        languageNames: "Greek",
+        symbol: "el"
+    },
+    {
+        languageNames: "Gujarati",
+        symbol: "gu"
+    },
+    {
+        languageNames: "Haitian Creole",
+        symbol: "ht"
+    },
+    {
+        languageNames: "Hausa",
+        symbol: "ha"
+    },
+    {
+        languageNames: "Hawaiian",
+        symbol: "haw"
+    },
+    {
+        languageNames: "Hebrew",
+        symbol: "he" 
+    },
+    {
+        languageNames: "Hindi",
+        symbol: "hi"
+    },
+    {
+        languageNames: "Hmong",
+        symbol: "hmn"
+    },
+    {
+        languageNames: "Hungarian",
+        symbol: "hu"
+    },
+    {
+        languageNames: "Icelandic",
+        symbol: "is"
+    },
+    {
+        languageNames: "Igbo",
+        symbol: "ig"
+    },
+    {
+        languageNames: "Indonesian",
+        symbol: "id"
+    },
+    {
+        languageNames: "Irish",
+        symbol: "ga"
+    },
+    {
+        languageNames: "Italian",
+        symbol: "it"
+    },
+    {
+        languageNames: "Japanese",
+        symbol: "ja"
+    },
+    {
+        languageNames: "Javanese",
+        symbol: "jw"
+    },
+    {
+        languageNames: "Kannada",
+        symbol: "kn"
+    },
+    {
+        languageNames: "Kazakh",
+        symbol: "kk"
+    },
+    {
+        languageNames: "Khmer",
+        symbol: "km"
+    },
+    {
+        languageNames: "Korean",
+        symbol: "ko"
+    },
+    {
+        languageNames: "Kurdish",
+        symbol: "ku"
+    },
+    {
+        languageNames: "Kyrgyz",
+        symbol: "ky"
+    },
+    {
+        languageNames: "Lao",
+        symbol: "lo"
+    },
+    {
+        languageNames: "Latin",
+        symbol: "la"
+    },
+    {
+        languageNames: "Latvian",
+        symbol: "lv"
+    },
+    {
+        languageNames: "Lithuanian",
+        symbol: "lt"
+    },
+    {
+        languageNames: "Luxembourgish",
+        symbol: "lb"
+    },
+    {
+        languageNames: "Macedonian",
+        symbol: "mk"
+    },
+    {
+        languageNames: "Malagasy",
+        symbol: "mg"
+    },
+    {
+        languageNames: "Malay",
+        symbol: "ms"
+    },
+    {
+        languageNames: "Malayalam",
+        symbol: "ml"
+    },
+    {
+        languageNames: "Maltese",
+        symbol: "mt"
+    },
+    {
+        languageNames: "Maori",
+        symbol: "mi"
+    },
+    {
+        languageNames: "Marathi",
+        symbol: "mr"
+    },
+    {
+        languageNames: "Mongolian",
+        symbol: "mn"
+    },
+    {
+        languageNames: "Myanmar(Burmese)",
+        symbol: "my"
+    },
+    {
+        languageNames: "Nepali",
+        symbol: "ne"
+    },
+    {
+        languageNames: "Norwegian",
+        symbol: "no"
+    },
+    {
+        languageNames: "Nyanja(Chichewa)",
+        symbol: "ny"
+    },
+    {
+        languageNames: "Pashto",
+        symbol: "ps"
+    },
+    {
+        languageNames: "Persian",
+        symbol: "fa"
+    },
+    {
+        languageNames: "Polish",
+        symbol: "pl"
+    },
+    {
+        languageNames: "Portuguese(Portugal, Brazil)",
+        symbol: "pt"
+    },
+    {
+        languageNames: "Punjabi",
+        symbol: "pa"
+    },
+    {
+        languageNames: "Romanian",
+        symbol: "ro"
+    },
+    {
+        languageNames: "Russian",
+        symbol: "ru"
+    },
+    {
+        languageNames: "Samoan",
+        symbol: "sm"
+    },
+    {
+        languageNames: "Scots Gaelic",
+        symbol: "gd"
+    },
+    {
+        languageNames: "Serbian",
+        symbol: "sr"
+    },
+    {
+        languageNames: "Sesotho",
+        symbol: "st"
+    },
+    {
+        languageNames: "Shona",
+        symbol: "sn"
+    },
+    {
+        languageNames: "Sindhi",
+        symbol: "sd"
+    },
+    {
+        languageNames: "Sinhala(Sinhalese)",
+        symbol: "si"
+    },
+    {
+        languageNames: "Slovak",
+        symbol: "sk"
+    },
+    {
+        languageNames: "Slovenian",
+        symbol: "sl"
+    },
+    {
+        languageNames: "Somali",
+        symbol: "so"
+    },
+    {
+        languageNames: "Spanish",
+        symbol: "es"
+    },
+    {
+        languageNames: "Sundanese",
+        symbol: "su"
+    },
+    {
+        languageNames: "Swahili",
+        symbol: "sw"
+    },
+    {
+        languageNames: "Swedish",
+        symbol: "sv"
+    },
+    {
+        languageNames: "Tagalog(Filipino)",
+        symbol: "tl"
+    },
+    {
+        languageNames: "Tajik",
+        symbol: "tg"
+    },
+    {
+        languageNames: "Tamil",
+        symbol: "ta"
+    },
+    {
+        languageNames: "Telugu",
+        symbol: "te"
+    },
+    {
+        languageNames: "Thai",
+        symbol: "th"
+    },
+    {
+        languageNames: "Turkish",
+        symbol: "tr"
+    },
+    {
+        languageNames: "Ukrainian",
+        symbol: "uk"
+    },
+    {
+        languageNames: "Urdu",
+        symbol: "ur"
+    },
+    {
+        languageNames: "Uzbek",
+        symbol: "uz"
+    },
+    {
+        languageNames: "Vietnamese",
+        symbol: "vi"
+    },
+    {
+        languageNames: "Welsh",
+        symbol: "cy"
+    },
+    {
+        languageNames: "Xhosa",
+        symbol: "xh"
+    },
+    {
+        languageNames: "Yiddish",
+        symbol: "yi"
+    },
+    {
+        languageNames: "Yoruba",
+        symbol: "yo"
+    },
+    {
+        languageNames: "Zulu",
+        symbol: "zu"
+    }
+];
 
+console.log(supportedLanguages.length);
+
+for (var i = 0; i < supportedLanguages.length; i++){
+    
+}
 
 // translator api stuff
-var selectedLanguage = "";  // should be something like en, jp, etc. for reference look at google translate api
-var translatedLanguage = "";    // same stuff from above
-var translatedText = "";    // any text you want to translate from sl to tl . . .
+var selectedLanguage = "en"; // should be something like en, jp, etc. for reference look at google translate api
+var translatedLanguage = "ja"; // same stuff from above
+var translatedText = "hello"; // any text you want to translate from sl to tl . . .
 var translateURL = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" + selectedLanguage + "&tl=" + translatedLanguage + "&dt=t&q=" + translatedText;
 
 $.ajax({
-    url: queryURL,
+    url: translateURL,
     method: "GET"
-}).then(function (response) {
+}).then(function (translator) {
+    console.log(translator[0][0][0]); // this is the tl response
+    console.log(translator[0][0][1]); // this is the sl response
+});
 
 
 // function functionality
